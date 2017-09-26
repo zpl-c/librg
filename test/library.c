a53434dfe70857df44daa124b47766fbe56269d1
Dominik Madarász <zaklaus@outlook.com>
2017-09-26 07:59:36 +0200

    Static library hackery, but it works now at least.

--- test/library.c
+++ test/library.c
@@ -1,3 +1,11 @@
 #define LIBRG_IMPLEMENTATION
 #define LIBRG_DEBUG
+
+// Windows stub, it is needed since CMake won't do this for us..
+//
+#ifdef WIN32
+#	pragma comment(lib, "ws2_32.lib")
+#	pragma comment(lib, "winmm.lib")
+#endif
+
 #include <librg.h>
