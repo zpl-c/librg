569d82ce5b55f90698a2dc7fab44a7ec18ab1281
Dominik Madarász <zaklaus@outlook.com>
2017-12-03 00:15:16 +0100

    Updated cmake batch script

--- scripts/cmake-vs2017.bat
+++ scripts/cmake-vs2017.bat
@@ -6,5 +6,5 @@ REM Make sure you also install SDL2.
 if not exist build mkdir build
 
 pushd build
-cmake -G "Visual Studio 15 2017 Win64" -DLIBRG_DEMO=1 -DLIBRG_CLI_TEST=1 -DVIDEO_OPENGLES=1 ..
+cmake -G "Visual Studio 15 2017 Win64" -DLIBRG_DEMO=1 -DLIBRG_CLI_TEST=1 -DVIDEO_OPENGLES=1 -DLIBRG_STATIC=1 ..
 popd
