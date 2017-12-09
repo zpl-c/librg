5ed354744d335fd092506a17901bcf648585ba55
Dominik Madarasz <zaklaus@outlook.com>
2017-12-09 01:58:12 +0100

    updated build-pkg

--- scripts/build-pkg.js
+++ scripts/build-pkg.js
@@ -14,20 +14,22 @@ const files = [
 ]
 
 const pkg = './pkg/'
+const pkg_includes = './pkg/include/'
 
 // recreate pkg dir
 fs.rmdirSync(pkg)
 fs.mkdirSync(pkg)
+fs.mkdirSync(pkg_includes)
 
 // copy header files
 libs.map(lib => fs.copyFileSync(
     './node_modules/' + lib[0] + '/include/' + lib[1],
-    pkg + lib[1]
+    pkg_includes + lib[1]
 ))
 
 // copy librg
-fs.copyFileSync('./include/librg.h', pkg + 'librg.h')
+fs.copyFileSync('./include/librg.h', pkg_includes + 'librg.h')
 
 // copy files else
-files.map(file => fs/copyFileSync(file, pkg + file))
+files.map(file => fs.copyFileSync(file, pkg + file))
 
