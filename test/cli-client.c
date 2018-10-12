38b5dac5541440dff6a8469a61d03a5c32791b42
Dominik Madarász <zaklaus@outlook.com>
2018-10-12 17:47:22 +0200

    coding style changes and bugfixes

--- test/cli-client.c
+++ test/cli-client.c
@@ -2,25 +2,25 @@
 #define LIBRG_DEBUG
 #include <librg.h>
 
-void on_connect_request(librg_event_t *event) {
+void on_connect_request(librg_event *event) {
     librg_data_wu32(event->data, 42);
     librg_log("on_connect_request\n");
 }
 
-void on_connect_accepted(librg_event_t *event) {
+void on_connect_accepted(librg_event *event) {
     librg_log("on_connect_accepted\n");
     librg_log("my entity: %u\n", event->entity->id);
 }
 
-void on_connect_refused(librg_event_t *event) {
+void on_connect_refused(librg_event *event) {
     librg_log("on_connect_refused\n");
 }
 
-void on_entity_create(librg_event_t *event) {
+void on_entity_create(librg_event *event) {
     // ...
 }
 
-void on_entity_update(librg_event_t *event) {
+void on_entity_update(librg_event *event) {
     // ...
 }
 
@@ -32,16 +32,14 @@ int main() {
                  "==================================================\n";
     librg_log("%s\n\n", test);
 
-    librg_option_set(LIBRG_NETWORK_BUFFER_SIZE, 2);
-
-    librg_ctx_t original     = {0};
+    librg_ctx original     = {0};
     original.tick_delay      = 100;
     original.mode            = LIBRG_MODE_CLIENT;
     original.world_size      = zpl_vec3f(5000.0f, 5000.0f, 0.f);
     original.max_entities    = 60000;
 
     #define size 1
-    librg_ctx_t *ctxs = zpl_malloc(size*sizeof(librg_ctx_t));
+    librg_ctx *ctxs = zpl_malloc(size*sizeof(librg_ctx));
 
     for (int i = 0; i < size; ++i) {
         ctxs[i] = original;
@@ -55,7 +53,7 @@ int main() {
         librg_event_add(&ctxs[i], LIBRG_ENTITY_CREATE, on_entity_create);
         librg_event_add(&ctxs[i], LIBRG_ENTITY_UPDATE, on_entity_update);
 
-        librg_network_start(&ctxs[i], (librg_address_t) { .host = "::1", .port = 7779 });
+        librg_network_start(&ctxs[i], (librg_address) { .host = "::1", .port = 7779 });
     }
 
     while (true) {
