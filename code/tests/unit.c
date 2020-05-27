bb551bc26b8530b1ff2146090cdec84a6e15ee66
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2020-05-27 13:40:28 +0300

    updated testing suite

--- code/tests/unit.c
+++ code/tests/unit.c
@@ -11,7 +11,8 @@ static int _g_errors = 0;
 #define MODULE(name, scope) \
     int ZPL_JOIN2(module__,name)() { \
         zpl_printf("--------------------------------------\n"); \
-        zpl_printf("  MODULE: %s\n", #name); \
+        zpl_printf(" group: %s\n", #name); \
+        zpl_printf("--------------------------------------\n"); \
         fflush(stdout); \
         _g_modules++; \
         int _total = 0; \
@@ -20,7 +21,7 @@ static int _g_errors = 0;
         char *_errstr = 0; \
         scope; \
         fflush(stdout); \
-        zpl_printf("  Results: %d total, %d failed, %d passed\n", _total, _errors, _total - _errors); \
+        zpl_printf("\n results: %d total, %s%d failed\x1B[0m, %s%d passed\x1B[0m\n", _total, _errors>0?"\x1B[31m":"", _errors, _errors==0?"\x1B[32m":"", _total - _errors); \
         _g_total += _total; \
         _g_errors += _errors; \
         if (_errors) _g_modules_err++; \
@@ -33,10 +34,10 @@ static int _g_errors = 0;
     _total += 1; \
     do scope while(0); \
     _errors += _lasterr; \
-    printf("    CASE: It %-80s: %s%s\n", desc, (_lasterr) ? "\x1B[31mFAIL\x1B[0m" : "\x1B[32mPASS\x1B[0m", _errstr);
+    printf(" * [%s]: It %s %s\n", (_lasterr) ? "\x1B[31mFAIL\x1B[0m" : "\x1B[32mPASS\x1B[0m", desc, _errstr);
 
-#define FAIL(a, b)                             { _errstr = zpl_bprintf("\n      Reason: %s:%d %s %s:%d\n", #a, (int)a, (a == b)?"==":"!=", #b, b); _lasterr = 1; break; }
-#define STRFAIL(a, b)                          { _errstr = zpl_bprintf("\n      Reason: %s:%s %s %s:%s\n", #a, (char *)a, (!strcmp(a,b))?"==":"!=", #b, b); _lasterr = 1; break; }
+#define FAIL(a, b)                             { _errstr = zpl_bprintf("\n\n\tassert: \x1B[31m%s:%d %s %s:%d\x1B[0m\n\tat %s:%d\n", #a, (int)a, (a == b)?"==":"!=", #b, b, __FILE__, __LINE__); _lasterr = 1; break; }
+#define STRFAIL(a, b)                          { _errstr = zpl_bprintf("\n\n\tassert: \x1B[31m%s:%s %s %s:%s\x1B[0m\n\tat %s:%d\n", #a, (char *)a, (!strcmp(a,b))?"==":"!=", #b, b, __FILE__, __LINE__); _lasterr = 1; break; }
 #define EQUALS(a, b)        if (a != b)        { FAIL(a, b); }
 #define STREQUALS(a, b)     if (!!strcmp(a,b)) { STRFAIL(a, b); }
 #define STRNEQUALS(a, b)    if (!strcmp(a,b))  { STRFAIL(a, b); }
@@ -78,22 +79,26 @@ static int _g_errors = 0;
 
 int main() {
     int err = 0, cnt = 0;
-    zpl_printf("librg unit tests\n");
+    zpl_printf("> testing suite:\n\n");
+    zpl_printf("   * library: librg\n");
 
     #define X(name) cnt++;
         MODULE_LIST
     #undef X
 
-    zpl_printf("Modules to be tested: %d\n", cnt);
+    zpl_printf("   * groups: %d\n", cnt);
+    zpl_printf("\n", cnt);
 
     #define X(name) err += ZPL_JOIN2(module__,name)();
         MODULE_LIST
     #undef X
 
     fflush(stdout);
-    zpl_printf("--------------------------------------\n");
-    zpl_printf("MODULES: %d total, %d failed, %d passed\n", _g_modules, _g_modules_err, _g_modules - _g_modules_err);
-    zpl_printf("TESTS:   %d total, %d failed, %d passed\n", _g_total, _g_errors, _g_total - _g_errors);
+    zpl_printf("--------------------------------------\n"); \
+    zpl_printf("> total:\n\n");
+    zpl_printf(" * modules: %d total, %s%d failed\x1B[0m, %s%d passed\x1B[0m\n", _g_modules, _g_modules_err>0?"\x1B[31m":"" ,_g_modules_err, _g_modules_err==0?"\x1B[32m":"", _g_modules - _g_modules_err);
+    zpl_printf(" * tests:   %d total, %s%d failed\x1B[0m, %s%d passed\x1B[0m\n", _g_total, _g_errors>0?"\x1B[31m":"" ,_g_errors, _g_errors==0?"\x1B[32m":"", _g_total - _g_errors);
+    zpl_printf("\n", cnt);
 
     return -err;
 }
