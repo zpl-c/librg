b9027f74f40acd77968876da3ecc817f76bc76ef
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2020-06-02 16:11:40 +0300

    disabled behavior methods

--- code/header/entity.h
+++ code/header/entity.h
@@ -55,8 +55,6 @@ LIBRG_API int8_t        librg_entity_radius_set(librg_world *, int64_t entity_id
 LIBRG_API int8_t        librg_entity_radius_get(librg_world *, int64_t entity_id);
 LIBRG_API int8_t        librg_entity_dimension_set(librg_world *, int64_t entity_id, int32_t dimension);
 LIBRG_API int32_t       librg_entity_dimension_get(librg_world *, int64_t entity_id);
-LIBRG_API int8_t        librg_entity_behavior_set(librg_world *, int64_t entity_id, librg_behavior key, int32_t value);
-LIBRG_API int32_t       librg_entity_behavior_get(librg_world *, int64_t entity_id, librg_behavior key);
 LIBRG_API int8_t        librg_entity_userdata_set(librg_world *, int64_t entity_id, void *data);
 LIBRG_API void *        librg_entity_userdata_get(librg_world *, int64_t entity_id);
 LIBRG_API int8_t        librg_entity_chunkarray_set(librg_world *, int64_t entity_id, const librg_chunk *chunks, size_t chunk_amount);
@@ -65,5 +63,10 @@ LIBRG_API int8_t        librg_entity_visibility_global_set(librg_world *, int64_
 LIBRG_API int8_t        librg_entity_visibility_global_get(librg_world *, int64_t entity_id);
 LIBRG_API int8_t        librg_entity_visibility_owner_set(librg_world *, int64_t entity_id, int64_t owner_id, librg_visibility value);
 LIBRG_API int8_t        librg_entity_visibility_owner_get(librg_world *, int64_t entity_id, int64_t owner_id);
+
+#if 0 /* for future releases */
+LIBRG_API int8_t        librg_entity_behavior_set(librg_world *, int64_t entity_id, librg_behavior key, int32_t value);
+LIBRG_API int32_t       librg_entity_behavior_get(librg_world *, int64_t entity_id, librg_behavior key);
+#endif
 
 LIBRG_END_C_DECLS
