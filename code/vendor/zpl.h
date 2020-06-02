b03cff9327725f9b75cd448f15d750e8168609c0
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2020-06-02 14:04:49 +0300

    zpl upd

--- code/vendor/zpl.h
+++ code/vendor/zpl.h
@@ -257,8 +257,8 @@ Version History:
 #define ZPL_H
 
 #define ZPL_VERSION_MAJOR 10
-#define ZPL_VERSION_MINOR 4
-#define ZPL_VERSION_PATCH 1
+#define ZPL_VERSION_MINOR 5
+#define ZPL_VERSION_PATCH 0
 #define ZPL_VERSION_PRE ""
 
 // file: zpl_hedley.h
